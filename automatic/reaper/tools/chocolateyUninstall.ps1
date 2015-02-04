d5922b239f860c72f1935ff48fc9943671265598
anchnk <anchnk@live.com>
2015-02-04 19:50:25 +0100

    Adding uninstall script and update version of ketarin autoupdate settings

---
+++ automatic/reaper/tools/chocolateyUninstall.ps1
@@ -0,0 +1,20 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+try {
+  $processor = Get-WmiObject Win32_Processor
+  $is64bit = $processor.AddressWidth -eq 64
+  if ($is64bit) {
+    $unpath = "$Env:ProgramFiles\Reaper (x64)\uninstall.exe"
+  } else {
+    $unpath = "${Env:ProgramFiles(x86)}\Reaper\uninstall.exe"
+  }
+
+  Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw
+}
