a26ecdf6e46877edd97c30ab8c2a0d58e92acb0c
Olivier Martin <anchnk@live.com>
2015-02-16 22:56:24 +0100

    adding netbeans jee choco automatic package

---
+++ automatic/netbeans-jee/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = '{{PackageName}}'
+$installerType = 'EXE'
+$url = '{{DownloadUrl}}'
+$silentArgs = '--silent'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
