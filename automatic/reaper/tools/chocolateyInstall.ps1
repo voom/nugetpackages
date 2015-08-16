dd385c7f1255996ae1a53c50e1a579765e706dc0
Olivier Martin <anchnk@live.com>
2015-08-16 21:41:27 +0200

    Reaper V5.0

--- automatic/reaper/tools/chocolateyInstall.ps1
+++ automatic/reaper/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = '{{PackageName}}'
-$installerType = 'exe'
+$fileType = 'exe'
 $url = '{{DownloadUrl}}'
 $url64 = '{{DownloadUrlx64}}'
 $silentArgs = '/S'
 $validExitCodes = @(0,1223)
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
--- automatic/reaper/tools/chocolateyUninstall.ps1
+++ automatic/reaper/tools/chocolateyUninstall.ps1
@@ -1,20 +1,14 @@
 ﻿$packageName = '{{PackageName}}'
-$installerType = 'exe'
+$fileType = 'exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
-try {
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  if ($is64bit) {
-    $unpath = "$Env:ProgramFiles\Reaper (x64)\uninstall.exe"
-  } else {
-    $unpath = "${Env:ProgramFiles(x86)}\Reaper\uninstall.exe"
-  }
-
-  Uninstall-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
-  Write-ChocolateySuccess "$packageName"
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw
+$processor = Get-WmiObject Win32_Processor
+$is64bit = $processor.AddressWidth -eq 64
+if ($is64bit) {
+  $unpath = "$Env:ProgramFiles\Reaper (x64)\uninstall.exe"
+} else {
+  $unpath = "${Env:ProgramFiles(x86)}\Reaper\uninstall.exe"
 }
+
+Uninstall-ChocolateyPackage "$packageName" "$fileType" "$silentArgs" "$unpath" -validExitCodes $validExitCodes
