349c1ead8187282a47a48c57842c93043bb65223
Olivier Martin DTSI/DSI <olivier4.martin@orange.com>
2016-10-10 16:46:54 +0200

    ST3 Automatic Script update to match chocolatey 0.10.2

--- automatic/sublimetext3/tools/chocolateyInstall.ps1
+++ automatic/sublimetext3/tools/chocolateyInstall.ps1
@@ -1,43 +1,71 @@
-﻿$package = 'SublimeText3'
+﻿# Variables
+$package = '{{PackageName}}'
 $build = '{{PackageVersion}}'
-$installFolder = 'Sublime Text 3'
 
-try {
+# Determine if Sublime Text 3 $build Version is installed
+#   - Return the path if found
+#   - Return $false if not
+function getInstallationDirectory($buildArg) {
 
-  $installedPath = (Join-Path "${Env:\ProgramFiles(x86)}" $installFolder),
+  $installFolder = 'Sublime Text 3'
+  $installedPath = [string]::empty
+  $found = $false
+
+  if(!${env:ProgramFiles(x86)}) {
+
+    # 32 bit machine
+    $installedPath = Join-Path $Env:ProgramFiles $installFolder;
+
+  } else {
+
+    # 64 bit machine
+    # Get the first path which is existing
+    $installedPath = (Join-Path "${Env:ProgramFiles(x86)}" $installFolder),
     (Join-Path $Env:ProgramFiles $installFolder) |
     ? { Test-Path $_ } |
     Select -First 1
 
-  # only way to test for installation of this version is by path on disk
-  $found = $false
-  if ($installedPath -and (Test-Path $installedPath))
-  {
+  }
+
+  # Check Sublime Text's folder existence
+  if (![string]::IsNullOrEmpty($installedPath) -and (Test-Path $installedPath)) {
+
     $exe = Join-Path $installedPath 'sublime_text.exe'
-    $version = (Get-Command $exe).FileVersionInfo.ProductVersion
-    $found = ($version -eq $build)
-  }
 
-  if ($found)
-  {
-    Write-Host "$package is already installed to $installedPath"
-  }
-  else
-  {
-    $params = @{
-      PackageName = $package;
-      FileType = 'exe';
-      #uses InnoSetup - http://www.jrsoftware.org/ishelp/index.php?topic=setupcmdline
-      SilentArgs = '/VERYSILENT /NORESTART /TASKS="contextentry"';
-      Url = "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%20$build%20Setup.exe";
-      Url64Bit = "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%20$build%20x64%20Setup.exe";
+    # Check Sublime Executable existence
+    if (![string]::IsNullOrEmpty($exe) -and (Test-Path $exe)) {
+      $version = (Get-Command $exe).FileVersionInfo.ProductVersion
+      $found = ($version -eq $buildArg)
     }
-
-    Install-ChocolateyPackage @params
   }
 
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
+  # If we don't found the build we are looking for
+  if (!$found) {
+    $installedPath = $false
+  }
+
+  return $installedPath;
+}
+
+# Self Executing utiliy function
+$result = (getInstallationDirectory($build))
+
+if ( !($result -eq $false) )  {
+
+  # Sublime is installed
+  Write-Host "$package is already installed to : " $result
+
+} else {
+
+  # Sublime is not installed
+  $params = @{
+    PackageName = $package;
+    FileType = 'exe';
+    #uses InnoSetup - http://www.jrsoftware.org/ishelp/index.php?topic=setupcmdline
+    SilentArgs = '/VERYSILENT /NORESTART /TASKS="contextentry"';
+    Url = '{{DownloadUrl}}';
+    Url64Bit = '{{DownloadUrlx64}}';
+  }
+
+  Install-ChocolateyPackage @params
 }
--- automatic/sublimetext3/tools/chocolateyUninstall.ps1
+++ automatic/sublimetext3/tools/chocolateyUninstall.ps1
@@ -1,26 +1,76 @@
-$package = 'SublimeText3'
-$installFolder = 'Sublime Text 3'
+# Variables
+$package = '{{PackageName}}'
+$build = '{{PackageVersion}}'
 
-try {
+# Determine if Sublime Text 3 $build Version is installed
+#   - Return the path if found
+#   - Return $false if not
+function getInstallationDirectory($buildArg) {
 
-  $path = ${Env:ProgramFiles(x86)}, $Env:ProgramFiles |
-    % { Join-Path $_ $installFolder } |
+  $installFolder = 'Sublime Text 3'
+  $installedPath = [string]::empty
+  $found = $false
+
+  if(!${env:ProgramFiles(x86)}) {
+
+    # 32 bit machine
+    $installedPath = Join-Path $Env:ProgramFiles $installFolder;
+
+  } else {
+
+    # 64 bit machine
+    # Get the first path which is existing
+    $installedPath = (Join-Path "${Env:ProgramFiles(x86)}" $installFolder),
+    (Join-Path $Env:ProgramFiles $installFolder) |
     ? { Test-Path $_ } |
     Select -First 1
 
-  if ($path)
-  {
-    Push-Location $path
-    $uninstaller = '.\unins000.exe'
-    if (Test-Path $uninstaller)
-    {
-      $unargs = '/silent', '/verysilent', '/suppressmsgboxes', '/norestart'
-      &$uninstaller $unargs
+  }
+
+  # Check Sublime Text's folder existence
+  if (![string]::IsNullOrEmpty($installedPath) -and (Test-Path $installedPath)) {
+
+    $exe = Join-Path $installedPath 'sublime_text.exe'
+
+    # Check Sublime Executable existence
+    if (![string]::IsNullOrEmpty($exe) -and (Test-Path $exe)) {
+      $version = (Get-Command $exe).FileVersionInfo.ProductVersion
+      $found = ($version -eq $buildArg)
     }
   }
 
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
+  # If we don't found the build we are looking for
+  if (!$found) {
+    $installedPath = $false
+  }
+
+  return $installedPath;
+}
+
+# Self Executing utiliy function
+$result = (getInstallationDirectory($build))
+
+if ( !($result -eq $false) )  {
+
+  # Sublime is installed Start the uninstallation
+  Push-Location $result # move to Sublime Text Folder
+  $uninstallerName = 'unins000.exe'
+
+  # Check if the Uninstaller is present in $result folder
+  if (Test-Path $uninstallerName) {
+
+    $params = @{
+      PackageName = $package;
+      FileType = 'exe';
+      SilentArgs = '/SILENT /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /TASKS="contextentry"';
+      File = (Join-Path $result $uninstallerName)
+    }
+
+   Uninstall-ChocolateyPackage @params
+
+  }
+} else {
+
+  # Sublime is not installed
+  Write-Host "Sublime Text $build is not installed"
 }
